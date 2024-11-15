d17c77689984a59f4afe95d7f6d8c638effae145
HarlanV <harlan.eq@gmail.com>
2024-11-15 14:24:35 -0300

    inclusão de instrução no comentario do main

--- main.py
+++ main.py
@@ -10,19 +10,22 @@ key_path = 'chave-bq.json'
 
 
 print("Iniciando processo...")
-# html_content = WebScraping(url, path).execute().get_html_file()
-html_content = WebScraping(url, path).get_html_file()
 
-print("html gerado!")
+# Caso tenha dado algum problema no processo da tabela, porem o html está correto: trocar as linhas abaixo
+html_content = WebScraping(url, path).execute().get_html_file()
+# html_content = WebScraping(url, path).get_html_file()
+
+print("Html gerado! Criando tabela...")
 
 soup = BeautifulSoup(html_content, 'html.parser')
 
-print("Soup gerado!")
-
 table = TableSteam(soup).get_data()
 
-print("Dataframe Gerado!")
+print("Tabela gerada. Exportando resultados...")
 
-table.to_excel("dados_steam.xlsx",sheet_name='dados_enviados')
+# descomentar abaixo para gerar dados locais em xlsx
+#table.to_excel("dados_steam.xlsx",sheet_name='dados_enviados')
+
+# comente abaixo caso não tenha acesso ao google bq
 export_status = ExportTableBQ(key_path).export_table(table)
-print("!! Processo Finalizado !!")
+print("Processo Finalizado !!")
